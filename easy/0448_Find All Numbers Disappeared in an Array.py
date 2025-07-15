a1c1e5b020b6c94e04de0d77f004b77f06f1ff1d
ykang <tuq33085@temple.edu>
2025-07-15 08:13:21 +0800

    refactor: restructure 0448.py into a folder

--- easy/0448_Find All Numbers Disappeared in an Array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        wyk = set()
-        for i in range(1, len(nums)+1):
-            wyk.add(i)
-        ly = set(nums)
-        return list(wyk - ly)
