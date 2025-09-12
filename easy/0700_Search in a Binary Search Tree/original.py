64166b1af0c83e327b01993c4e57d7d78b84cd21
ykang <tuq33085@temple.edu>
2025-09-11 21:07:54 -0400

    Add new folder for LC #700 with original and optimized solutions

---
+++ easy/0700_Search in a Binary Search Tree/original.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        if root.val == val:
+            return root
+        wyk = self.searchBST(root.left, val)
+        ly = self.searchBST(root.right, val)
+        if wyk is None and ly is None:
+            return None
+        if wyk is not None:
+            return wyk
+        if ly is not None:
+            return ly
+            
