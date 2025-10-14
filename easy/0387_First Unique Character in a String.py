3280d22b19744096f5c428e6f7b3bc46b1f2e88a
ykang <tuq33085@temple.edu>
2025-10-13 21:01:16 -0400

    remove old LC #387 file

--- easy/0387_First Unique Character in a String.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        for wyk in s:
-            if wyk not in s[s.index(wyk)+1:]:
-                return s.index(wyk)
-        return -1
-                
-
-        
